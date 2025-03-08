65d5855368dd38590f7c67d9767ff8a7cafffd8d
mv735065 <245119735065@mvsrec.edu.in>
2025-03-08 19:25:52 +0530

    drill completed, added files

---
+++ README.MD
@@ -0,0 +1,132 @@
+# Sprint Challenge: SQL
+
+Design a database for an online chat system.
+
+## Deliverables
+
+Add a file called `queries.sql` that runs all of the `CREATE TABLE`,
+`INSERT`, and `SELECT` queries, below.
+
+The last question is a fill-in-the-blank. You can add that as a SQL
+comment to the end of `queries.sql`.
+
+## Details
+
+The chat system has an arbitrary number of:
+
+* Organizations (e.g. `Lambda School`)
+* Channels (e.g. `#random`)
+* Users (e.g. `Dave`)
+
+The following relationships exist:
+
+* An organization can have many channels.
+* A channel can belong to one organization.
+* A channel can have many users subscribed.
+* A user can be subscribed to many channels.
+* Additionally, a user can post messages to a channel. (Note that a user might have
+posted messages to a channel to which they subscribed in the past, but they no
+longer subscribe to now.)
+
+In the following, there will be more columns that you have to add in
+various tables, not just the columns listed here.
+
+1. Write `CREATE TABLE` statements for tables `organization`, `channel`, `user`,
+   and `message`.
+
+   1. `organization`. This table should at least have column(s):
+      * `name`
+
+   2. `channel`. This table should at least have column(s):
+      * `name`
+
+   3. `user`. This table should at least have column(s):
+      * `name`
+
+   4. `message`. This table should have at least columns(s):
+
+      * `post_time`--the timestamp of when the message was posted
+        * See [Date types in
+          SQLite](https://www.sqlite.org/datatype3.html#date_and_time_datatype).
+          Also see the SQLite function `datetime()`.
+
+      * `content`--the message content itself
+
+2. Add additional foreign keys needed to the above tables, if any.
+
+3. Add additional join tables needed, if any.
+
+4. Write `INSERT` queries to add information to the database.
+
+   For these `INSERT`s, it is OK to refer to users, channels, and organization
+   by their `id`s. No need to do a subselect unless you want to.
+
+   1. One organization, `Lambda School`
+   2. Three users, `Alice`, `Bob`, and `Chris`
+   3. Two channels, `#general` and `#random`
+   4. 10 messages (at least one per user, and at least one per channel).
+   5. `Alice` should be in `#general` and `#random`.
+   6. `Bob` should be in `#general`.
+   7. `Chris` should be in `#random`.
+
+5. Write `SELECT` queries to:
+
+   For these `SELECT`s, it is **NOT** OK to refer to users, channels, and
+   organization by their `id`s. You must join in those cases.
+
+   1. List all organization `name`s.
+
+   2. List all channel `name`s.
+
+   3. List all channels in a specific organization by organization `name`.
+
+   4. List all messages in a specific channel by channel `name` `#general` in
+      order of `post_time`, descending. (Hint: `ORDER BY`. Because your
+      `INSERT`s might have all taken place at the exact same time, this might
+      not return meaningful results. But humor us with the `ORDER BY` anyway.)
+
+   5. List all channels to which user `Alice` belongs.
+
+   6. List all users that belong to channel `#general`.
+
+   7. List all messages in all channels by user `Alice`.
+
+   8. List all messages in `#random` by user `Bob`.
+
+   9. List the count of messages across all channels per user. (Hint:
+      `COUNT`, `GROUP BY`.)
+      
+      The title of the user's name column should be `User Name` and the title of
+      the count column should be `Message Count`. (The SQLite commands
+	  `.mode column` and `.header on` might be useful here.)
+
+      The user names should be listed in reverse alphabetical order.
+      
+      Example:
+
+      ```
+      User Name   Message Count
+      ----------  -------------
+      Chris       4
+      Bob         3
+      Alice       3
+      ```
+
+   10. [Stretch!] List the count of messages per user per channel.
+
+       Example:
+
+       ```
+       User        Channel     Message Count
+       ----------  ----------  -------------
+       Alice       #general    1
+       Bob         #general    1
+       Chris       #general    2
+       Alice       #random     2
+       Bob         #random     2
+       Chris       #random     2
+       ```
+
+6. What SQL keywords or concept would you use if you wanted to automatically
+   delete all messages by a user if that user were deleted from the `user`
+   table?
---
+++ queries.sql
@@ -0,0 +1,159 @@
+
+
+show tables ;
+
+create database CHAT_SYSTEM;
+
+create table organization (
+id int auto_increment primary key,
+name text);
+
+create table channel (
+id int auto_increment primary key,
+name text);
+
+ALTER TABLE channel
+ADD COLUMN organization_id INT;
+
+alter table channel
+add foreign key (organization_id) references organization(id);
+
+create table user (
+id int auto_increment primary key,
+name text);
+
+CREATE TABLE Channel_Users (
+   user_id INT,
+   channel_id INT,
+   PRIMARY KEY (user_id, channel_id),
+   FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE,
+   FOREIGN KEY (channel_id) REFERENCES channel(id) ON DELETE CASCADE
+);
+
+create table message (
+id int auto_increment primary key,
+post_time datetime,
+user_id int,
+channel_id int,
+content text ,
+FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE,
+   FOREIGN KEY (channel_id) REFERENCES channel(id) ON DELETE CASCADE
+);
+
+
+
+insert into organization(name) values('Lambda School');
+insert into user(name) values('Alice'),('Bob'),('Chris');
+insert into channel(name) values('#general'),('#random');
+
+update  channel
+set organization_id=1;
+
+INSERT INTO message
+       (post_time,user_id, channel_id, content)
+        VALUES
+(now(),(SELECT id FROM user WHERE name = 'Alice'), (SELECT id FROM channel WHERE name = '#general'), "Hey guys! I'm Alice"),
+  (NOW(), (SELECT id FROM user WHERE name = 'Alice'), (SELECT id FROM channel WHERE name = '#random'), "Hello buddies! I'm Alice"),
+  (NOW(), (SELECT id FROM user WHERE name = 'Bob'), (SELECT id FROM channel WHERE name = '#general'), "Hey guys! I'm Bob"),
+  (NOW(), (SELECT id FROM user WHERE name = 'Alice'), (SELECT id FROM channel WHERE name = '#general'), "Hello Bob!"),
+  (NOW(), (SELECT id FROM user WHERE name = 'Chris'), (SELECT id FROM channel WHERE name = '#random'), "Hey guys! I'm Chris"),
+  (NOW(), (SELECT id FROM user WHERE name = 'Bob'), (SELECT id FROM channel WHERE name = '#general'), "That's Great!"),
+  (NOW(), (SELECT id FROM user WHERE name = 'Chris'), (SELECT id FROM channel WHERE name = '#random'), "Let's play a game"),
+  (NOW(), (SELECT id FROM user WHERE name = 'Alice'), (SELECT id FROM channel WHERE name = '#random'), "I think playing online games is boring"),
+  (NOW(), (SELECT id FROM user WHERE name = 'Alice'), (SELECT id FROM channel WHERE name = '#general'), "Yeah! we shall meet today at Cafe Coffee Day"),
+  (NOW(), (SELECT id FROM user WHERE name = 'Bob'), (SELECT id FROM channel WHERE name = '#general'), "Okay!");
+
+
+INSERT INTO Channel_Users (user_id,channel_id)
+VALUES 
+  ((SELECT id FROM user WHERE name = 'Alice'),(SELECT id FROM channel WHERE name = '#general')),
+  ((SELECT id FROM user WHERE name = 'Alice'),(SELECT id FROM channel WHERE name = '#random')),
+  ((SELECT id FROM user WHERE name = 'Bob'),(SELECT id FROM channel WHERE name = '#general')),
+  ((SELECT id FROM user WHERE name = 'Chris'),(SELECT id FROM channel WHERE name = '#random'));
+  
+--   List all organization names.
+select * from organization;
+
+-- List all channel names.
+select * from channel;
+
+-- List all channels in a specific organization by organization name
+select * from channel 
+inner join organization 
+on channel.organization_id=organization.id 
+where organization.name like 'Lambda%';
+
+-- List all messages in a specific channel by channel name #general in order of post_time, descending
+select message.content,channel.name,message.post_time from message
+ inner join 
+ channel 
+ on message.channel_id=channel.id
+ where channel.name='#general' 
+ order by message.post_time desc;
+
+-- List all channels to which user Alice belongs.
+select channel.name from channel 
+inner join 
+(
+select Channel_Users.channel_id as id from Channel_Users 
+inner join user on user.id=Channel_Users.user_id where user.name like 'Alice%'
+) 
+as temp 
+on temp.id=channel.id ;
+
+-- List all users that belong to channel #general
+select * from user 
+join 
+Channel_Users 
+on Channel_Users.user_id=user.id 
+join 
+channel 
+on Channel_Users.channel_id=channel.id
+ where channel.name like '#general%';
+
+-- List all messages in all channels by user Alice
+select message.channel_id,message.content,user.name from message 
+join 
+user on user.id=message.user_id 
+where user.name like 'Alice';
+
+-- List all messages in #random by user Bob.
+select message.channel_id,message.content,user.name from message 
+join 
+user 
+on user.id=message.user_id  
+join 
+channel 
+on channel.id=message.channel_id 
+where channel.name like '#random%' 
+AND user.name like 'Bob%';
+
+-- List the count of messages across all channels per user. (Hint: COUNT, GROUP BY.)
+select user.name as 'User Name',count(*) as 'Message Count' from message 
+join 
+user 
+on user.id=message.user_id  
+join 
+channel 
+on channel.id=message.channel_id 
+group by user.name ;
+
+-- List the count of messages per user per channel.
+select user.name as 'User',channel.name as 'Channel',count(*) as 'Message Count' from message 
+join 
+user 
+on user.id=message.user_id  
+join 
+channel 
+on channel.id=message.channel_id 
+group by user.name,channel.name;
+
+-- What SQL keywords or concept would you use if you wanted to automatically delete all messages by a user if that user were deleted from the user table?
+-- ANS: To automatically delete all messages associated with a user when that user is deleted from the user table,
+--  you would use foreign key constraints with the "ON DELETE CASCADE" option.
+
+select * from user;
+select * from message;
+
+
+select * from Channel_Users;
